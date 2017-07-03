0127c1e8ed4a2acd8eaab97e71d1045b1028635d
github username <github email address>
2017-07-03 10:33:37 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'static#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
